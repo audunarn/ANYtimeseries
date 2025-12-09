feb8ba232a23d5ecf816758ab5f11c094d71bbf5
Audun Arnesen Nyhus <audunarn@gmail.com>
2025-12-09 09:28:52 +0100

    Handle flexible time coordinates in NetCDF loader

--- tests/test_file_loader.py
+++ tests/test_file_loader.py
@@ -190,3 +190,25 @@ def test_load_era5_netcdf_single_point(monkeypatch, tmp_path):
     assert data["u10"].t.shape[0] == times.size
     assert data["u10"].dtg_ref == times[0].to_pydatetime()
     np.testing.assert_array_equal(data["mwd"].x, np.linspace(0.0, 30.0, times.size))
+
+
+def test_load_netcdf_flexible_time_detection(monkeypatch, tmp_path):
+    file_loader = _load_file_loader(monkeypatch)
+
+    times = pd.date_range("2021-06-01", periods=3, freq="D")
+    ds = xr.Dataset(
+        {"temp": ("forecast_time", np.linspace(5.0, 7.0, times.size))},
+        coords={"forecast_time": times},
+    )
+    nc_path = tmp_path / "forecast_sample.nc"
+    ds.to_netcdf(nc_path)
+
+    loader = file_loader.FileLoader()
+    tsdb = loader._load_generic_file(str(nc_path))
+
+    data = tsdb.getm()
+    assert set(data) == {"temp"}
+    np.testing.assert_array_equal(data["temp"].t, np.array([0.0, 86400.0, 172800.0]))
+    np.testing.assert_array_equal(data["temp"].x, np.linspace(5.0, 7.0, times.size))
+    assert data["temp"].dtg_ref == times[0].to_pydatetime()
+    np.testing.assert_array_equal(data["temp"].dtg_time, times.to_pydatetime())
